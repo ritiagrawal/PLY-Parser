4833ca45c1e71912a7322165de68ca26451cadbf
ritiagrawal <ritiagrawal026@gmail.com>
2017-09-12 23:20:50 +0530

    astGeneration handling var and multiple var declaration error

--- Parser.py
+++ Parser.py
@@ -1,341 +1,177 @@
-# -----------------------------------------------------------------------------
-#Author : Rakshat Ranjan and Alok Kumar
-# -----------------------------------------------------------------------------
 import ply.yacc as yacc
+import sys
+arg1 = sys.argv[1]        # For taking input from command line.
 
 # Get the token map from the lexer.  This is required.
 from Scanner import tokens
+from astgenrator import *
 
-from astgenerator import *	#import from ast file
-
+class Expr:
+    def __init__(self, value, type):
+        self.type= type
+        self.value=value
+	
 precedence = (
-	('left','OR'),
-	('left','AND'),
-	('left', 'NE', 'EQ'),
-    ('left', 'GT', 'GE', 'LT', 'LE'),
     ('left', '+', '-'),
-    ('left', '*', '/'),
-    ('right', 'UMINUS'),
-    ('left','(',')'),
-    ('left','ELSE'),
-    ('right', 'NT'),
 )
 
-
-
+#defines the starting grammar rule (top level rule)
 start = 'program'
 
-
+#handle empty productions
 def p_empty(p):
     'empty :'
     pass
 
-#Print the final icode in icode.txt
+#following is the grammar
 def p_program(p):
-	"program : declaration_list procedure_definition"
-	p[0] = ProgramAst(p[2])
-	print >> filebuff, p[2].code
-
-def p_declarationlist(p):
-	'''declaration_list : procedure_declaration
-					    | variable_declaration_list procedure_declaration
-					    | procedure_declaration variable_declaration_list'''
-
-
-def p_proceduredecl(p):
-	'''procedure_declaration : VOID NAME '(' ')' ';' '''
-	if p[2] != "main":
-		print ("Procedure Name must be main")
-		raise SyntaxError
-	else:
-		symtab['void'].append(p[2])	#'main' variable stored in symbol table
-
-
-#procedure_definition check if main in symbol table
+    "program : procedure_definition"
+    print ("Sucessfully parsed")
+	
 def p_proceduredefn(p):
-	'''procedure_definition : NAME '(' ')' '{' optional_variable_declaration_list ex_statement_list '}' '''
-	if p[1] in symtab['void']:
-		p[0] = ProcedureAst(p[1],p[5],p[6],"")
-		p[0].code = "Procedure : main \n" + p[6].code
-	else:
-		print ("Procedure Name must be main")
-		raise SyntaxError
-
-def p_optvardecllist(p):
-	'''optional_variable_declaration_list : empty
-										  | variable_declaration_list'''
-	if p[1] == 'empty':
-		p[0] = NULL
-	else:
-		p[0] = p[1]
-
-# adding symbol table entries in symbol table
-def p_vardecllist(p):
-	'''variable_declaration_list : variable_declaration '''
-	for vaar in flotnames:
-		symtab['float'].append(vaar)
-	for vaar in intnames:
-		symtab['int'].append(vaar)
-
-def p_vardeclistlist(p):
-	''' variable_declaration_list : variable_declaration_list variable_declaration'''
-	for vaar in flotnames:
-		symtab['float'].append(vaar)
-	for vaar in intnames:
-		symtab['int'].append(vaar)
-
-
-# check for multiple declaration
-def p_vardecl(p):
-	''' variable_declaration : FLOAT var_list ';'
-							 | INT var_list ';' '''
-	global flotnames
-	global intnames
-	if p[1] == 'float':
-		for ard in varvar:
-			if ard in flotnames or ard in intnames:
-				print ("multiple declaration '%s' " %(ard))
-				raise SyntaxError
-			else:
-				flotnames.append(ard)
-		varvar[:] = []
-	elif p[1] == 'int':
-		for ard in varvar:
-			if ard in intnames or flotnames:
-				print ("multiple declaration '%s' " %(ard))
-				raise SyntaxError
-			else:
-				intnames.append(ard)
-		varvar[:] = []
-
-
-# check for multiple declaration
+    """procedure_definition : NAME '(' parameter_list ')' '{'  variable_declaration_list ex_statement_list return_stat '}' procedure_definition 
+							 | empty
+    """
+    #if len(p) == 2:
+        #print("Printinhg")
+			
+def p_parameterlist(p):
+    ''' parameter_list : VAR pointer_variable parameter_list
+						 | pointer_variable parameter_list
+						 | ',' parameter_list
+						 | empty '''
+    #if len(p) == 4:
+        #print(p[1])
+    
+def p_variabledeclarationlist(p):
+    ''' variable_declaration_list : empty
+								   | variable_declaration_list variable_declaration	'''
+    
+						 
+def p_variabledeclaration(p):
+    ''' variable_declaration : VAR var_list ';'
+							 '''
 def p_varlist(p):
-	''' var_list : NAME '''
-	#p[0] = p[1]
-	if p[1] not in varvar:
-		varvar.append(p[1])
-	else :
-		print ("Multiple declarations '%s' ") %(p[1])
-		raise SyntaxError
-
-
-def p_varli(p):	
-	''' var_list : var_list ',' NAME '''
-	if p[3] not in varvar:
-		varvar.append(p[3])
-	else :
-		print ("Multiple declarations '%s' ") %(p[3])
-		raise SyntaxError
-
-
-def p_exstatlistemp(p):
-	''' ex_statement_list : empty'''
-	p[0] = SequenceAst("")
-
-def p_exstatstat(p):
-	''' ex_statement_list : ex_statement_list statement '''
-	p[1].pushbach(p[2])		#SequenceAst().pushbach() function defined in astgenerator
-	p[0]= p[1]
-	p[0].code += p[2].code 	#icode of ast's are concatenated
-
+    '''var_list : pointer_variable
+				| var_list ',' pointer_variable'''
+				
+    if len(p) == 2:
+        if p[1] not in varnames:
+            varnames.append(p[1]) 
+            print ("varnames",varnames)
+        else:
+            print ("Multiple declarations",p[1])
+    else:
+        if p[3] not in varnames:
+            varnames.append(p[3])
+        else:
+            print ("Multiple declarations",p[3])
+	
+def p_pointervariable(p):
+    ''' pointer_variable :  POINTER_OP pointer_variable
+						  | variable '''
+    if len(p)==2:
+        p[0]=p[1]					 
+    else:
+        p[0]=p[2]
+		
+def p_exstatlist(p):
+	''' ex_statement_list : empty
+						  | ex_statement_list statement '''
 
 def p_stmt(p):
 	''' statement : assignment_statement
-				  | if_else_statement
-				  | while_statement
-				  | compound_statement '''
-	p[0] = p[1]
-
-def p_compstmt(p):
-	''' compound_statement : '{' ex_statement_list '}' '''
-	p[0] = p[2]
-
-
-
+				   | cond_goto
+	               | use_stat
+				   | label
+				   | uncond_goto 
+				   | procedure_call '''
+				  
 def p_asgnstmt(p):
 	''' assignment_statement : variable ASSIGN_OP arith_expression ';'
-							 | variable ASSIGN_OP cond_comp ';' '''
-	if p[1].type != p[3].type:
-		print("Type mismatch Line no:%s"%(p.lineno(2)))		# type-check   
-		raise SyntaxError				#to ensure no output generated 
+							 | variable ASSIGN_OP POINTER_OP variable ';'
+							 | variable ASSIGN_OP ADDRESS_OP variable ';' '''
+	if len(p)==4:
+		p[0]=AssigOpArth(p[1],p[3])
+		p[0].place=""
+		p[0].code=""
 	else:
-		global interVar
-		p[0] = AssignAst(p[1],p[3],"","") 
-		p[0].place = "var"+str(interVar)	#new variable generated
-		interVar+=1
-		p[0].code = p[1].code + p[3].code + "\t%s = %s \n"%(p[1].place,p[3].place)	#related icode
-
-
-def p_arithexprbin(p):
+		p[0]=AssigOpArth(p[1],p[4])
+		p[0].place=""
+		p[0].code=""
+							
+def p_arithexpr(p):
 	''' arith_expression : arith_expression '+' arith_expression
 						 | arith_expression '-' arith_expression
-						 | arith_expression '*' arith_expression
-						 | arith_expression '/' arith_expression'''
-						 
-	global interVar
-	if p[1].type != p[3].type:			#type check
-		print("Type mismatch Line no:%s"%(p.lineno(2)))
-		raise SyntaxError
+						 | expression_term '''
+	if len(p)==2:
+		p[0]=p[1]
+	if len(p)==4:
+		p[0]=ArithOpAst(p[1],p[2],p[3])
+		p[0].place=""
+		p[0].code=""
+		print(p[0])
+    		
 
-	elif p[2]=='+' or p[2]=='-' or p[2]=='*' or p[2]=='/' :
-		p[0]= ArithOpAst(p[1],p[2],p[3],"","")
-		p[0].place = "var"+str(interVar)
-		interVar+=1
-		p[0].code = p[1].code + p[3].code + "\t%s = %s %s %s\n"%(p[0].place,p[1].place,p[2],p[3].place)
+def p_exprtermvar(p):
+	'''expression_term : variable'''
+	p[0]=NameAst(p[1])
+	
+def p_exprtermconstant(p):
+	'''expression_term : constant '''
+	p[0]=NumberAst(p[1])
 
+def p_exprtermprocedure(p):
+	'''expression_term : procedure_call'''
+	
+def p_condgoto(p):
+	''' cond_goto : IF '(' ')' uncond_goto '''
 
-def p_arithexpunary(p):
-	''' arith_expression : '-' arith_expression %prec UMINUS '''
-	global interVar
-	p[0] = UnaryAst(p[2],"","")
-	p[0].place = "var"+str(interVar)
-	interVar+=1
-	p[0].code = p[2].code  + "\t%s = 'uminus' %s\n"%(p[0].place,p[2].place)
-
-
-def p_arithbrkt(p):
-	''' arith_expression : '(' arith_expression ')' '''
-	p[0] = p[2]
-
-def p_arithexpterm(p):
-	''' arith_expression : expression_term '''
-	p[0] = p[1]
-
-def p_logexpr(p):
-	''' logic_expression : arith_expression EQ arith_expression
-						 | arith_expression NE arith_expression
-						 | arith_expression GT arith_expression
-						 | arith_expression GE arith_expression
-						 | arith_expression LT arith_expression
-						 | arith_expression LE arith_expression'''
-
-	if p[1].type != p[3].type:
-		print("Type mismatch Line no:%s"%(p.lineno(2)))
-		raise SyntaxError
-		
-	else:
-		global interVar
-		p[0] = RelationAst(p[1],p[2],p[3],"","")
-		p[0].place = "var"+str(interVar)
-		interVar+=1
-		p[0].code = p[1].code + p[3].code + "\t%s := %s %s %s \n"%(p[0].place,p[1].place,p[2],p[3].place)
-
-def p_logexp(p):
-	''' logic_expression : logic_expression AND logic_expression
-						 | logic_expression OR logic_expression'''
-	global interVar
-	p[0] = RelationAst(p[1],p[2],p[3],"","")
-	p[0].place = "var"+str(interVar)
-	interVar+=1
-	p[0].code = p[1].code + p[3].code + "\t%s := %s %s %s \n"%(p[0].place,p[1].place,p[2],p[3].place)
-
-
-def p_lognot(p):
-	''' logic_expression : NT logic_expression
-						 | '(' logic_expression ')' '''
-	global interVar
-	if len(p) == 3:			#first production
-		p[0] = NotAst(p[2],"","")
-		p[0].place = "var"+str(interVar)
-		interVar+=1
-		p[0].code = p[2].code  + "\t%s = 'NOT' %s\n"%(p[0].place,p[2].place)
-	elif len(p) == 4:		#second production
-		p[0] = p[2]
-
-
-def p_ifelse(p):
-	''' if_else_statement : IF '(' logic_expression ')' statement
-						  | IF '(' logic_expression ')' statement ELSE statement '''
-	global label1
-	global label2
-	if len(p) == 6:
-		delta = SequenceAst("")
-		p[0]= SelectionAst(p[3],p[5],delta,"","")
-		p[0].code = "\n " + p[3].code + "\n\tif %s is false, goto Label0%d\n"%(p[3].place,label1) + p[5].code + "\n\tGoto Label1%d \nLabel 0%d:\n"%(label2,label1)  + "\nLabel1%d:\n"%(label2) 
-		label2+=1
-		label1+=1
-	elif len(p) == 8:
-		p[0]= SelectionAst(p[3],p[5],p[7],"","")
-		p[0].code = "\n " + p[3].code + "\n\tif %s is false, goto Label0%d\n"%(p[3].place,label1) + p[5].code + "\n\tGoto Label1%d \nLabel 0%d:\n"%(label2,label1)  + p[7].code + "\nLabel1%d:\n"%(label2)
-		label2+=1
-		label1+=1
-
-def p_condcomp(p):
-	''' cond_comp : logic_expression '?' arith_expression ':' arith_expression '''
-	global interVar
-	global label1
-	global label2
-	if p[3].type != p[5].type:
-		print("Type mismatch Line no:%s"%(p.lineno(2)))
-		raise SyntaxError
-	else:
-		p[0] = ConditionAst(p[1],p[3],p[5])
-		p[0].place = "var"+str(interVar)
-		interVar+=1
-		p[0].code = "\n " + p[1].code + "\n\tif %s is false, goto Label0%d\n"%(p[1].place,label1) + p[3].code + "\t%s = %s"%(p[0].place,p[3].place) + "\n\tGoto Label1%d \nLabel 0%d:\n"%(label2,label1)  + p[5].code + "\t%s = %s"%(p[0].place,p[5].place) + "\nLabel1%d:\n"%(label2)
-		label2+=1
-		label1+=1
-
-def p_whilec(p):
-	''' while_statement : WHILE '(' logic_expression ')' statement
-						| DO statement WHILE '(' logic_expression ')' ';' '''
-	global label1
-	global label2
-	if len(p) == 6:
-		p[0] = IterationAst(p[3],p[5],'false',"","")
-		p[0].code =  "Label 0%d:\n"%(label1) + p[3].code + "\n\tif %s is false, goto Label1%d \n"%(p[3].place,label2) + p[5].code + "\tGoto label0%d"%(label1) +"\nLabel 1%d:\n"%(label2)
-		label2+=1
-		label1+=1  
-	elif len(p) == 8:
-		p[0] = IterationAst(p[5],p[2],'true',"","")
-		p[0].code =  "Label 0%d:\n"%(label1) + p[2].code +  p[5].code + "\n\tif %s is true, goto Label0%d \n"%(p[5].place,label1)
-		label2+=1
-		label1+=1
-
-
-def p_exprterm(p):
-	'''expression_term : variable '''
-	p[0] = p[1]
-
-
-def p_exprtermcons(p):
-	''' expression_term	: constant '''
-	p[0] = p[1]
-
-        
-
+def p_procedurecall(p): 
+    ''' procedure_call : NAME '(' parameter_list ')' ';' '''
+	
 def p_var(p):
-	''' variable : NAME '''
-	global interVar
-	if p[1] in intnames or p[1] in flotnames:
-		p[0] = NameAst(p[1])
-		p[0].place = p[1]
-		p[0].code = ""
-	else:
-		print ("Undeclared name '%s'" %(p[1]))		#undeclared variable error
-		raise SyntaxError
-		
+	''' variable : NAME
+				  | arr_var
+	'''	
+	p[0]=p[1]
+	#p[0]=NameAst(p[1])
+	
 
-
+def p_arrayvar(p):
+    ''' arr_var : NAME '[' NUM ']' '''		  
+    p[0]=p[1]
+	
 def p_cons(p):
 	''' constant : NUM '''
-	global interVar
 	p[0] = NumberAst(p[1],"int")
-	p[0].place = p[1]
-	p[0].code = ""
+				 
+def p_returnstat(p):
+    ''' return_stat : RETURN pointer_variable ';'
+					| RETURN ADDRESS_OP variable ';'
+					| empty '''
 
-def p_consf(p):
-	''' constant : FNUM '''
-	global interVar
-	p[0] = NumberAst(p[1],"float")
-	p[0].place = p[1]
-	p[0].code = "" 
-
+def p_usestat(p):
+    ''' use_stat : USE '(' pointer_variable ')' ';' '''
+				
+def p_label(p):
+    ''' label : '<' BB NUM '>' ':' '''
+    p[0]=p[2]
+	
+def p_ungoto(p):
+    ''' uncond_goto : GOTO label '''
+				
+#for error handling
 def p_error(p):
-    print("Syntax error at '%s'  Line no: '%d' " %(p.value,p.lineno))
+    print("Syntax error at '%s'  '%d' " %(p.value,p.lineno))
 
+#import ply.yacc as yacc
 parser = yacc.yacc()
 
+with open(arg1, 'r') as myfile:
+    data=myfile.read()
 
-parser.parse(data)
+result= parser.parse(data)
+
+
+		
--- astgenerator.py
+++ astgenerator.py
@@ -1,25 +1,11 @@
-# -----------------------------------------------------------------------------
-#Author : Rakshat Ranjan and Alok Kumar
-# -----------------------------------------------------------------------------
 import sys
 arg1 = sys.argv[1]        # For taking input from command line.
 with open(arg1, 'r') as myfile:
     data=myfile.read()
 
-outfile =  arg1.replace('parse', 'icode')
-outfile = outfile.replace('.c', '.c.ic')
-filebuff = open(outfile, 'w')	# output file for icode, can be found in directory testcases/icode
-outfile2 = outfile.replace('icode','ast')
-outfile2 = outfile2.replace('.c.ic','.c.ast')
-filebuf = open(outfile2, 'w')	#output file for ast, can be found in directory testcases/ast
-
-
-# variables will be first stored in varvar, then stored in intnames or floatnames depending on 
-# whether FLOAT or INT comes before variable list, then accordingly will be stored in symbol table
-intnames = []
-flotnames = []
-varvar = []
-
+	
+varnames=[]	
+	
 #SymbolTable
 symtab = {
 	'int': [],
@@ -35,250 +21,33 @@ varStr = "var"
 #For labels in icode
 label1 = 0
 label2 = 0
-#For purpose of ast file inendation
-sequenceString = "    "
-astString = "       "
-subastString = "          "
-
-#Base class
-class Expr: pass
-
-#For arithmetic operators
-class ArithOpAst(Expr):
-    global astString
-    global subastString
-    def __init__(self,left,op,right,place,code):		#constructor
-        
-        self.left = left
-        self.right = right
-        self.op = op
-        if self.left.type == 'int':
-        	self.type  = 'int'
-        elif self.left.type == 'float':
-        	self.type = 'float'
-       	self.code = code
-        self.place = place
-
-    def numprint(self):				#print function
-    	print >> filebuf, "\n %s Arith: %s \n %s LHS (" % (astString,self.op, subastString),
-    	self.left.numprint()
-    	print >> filebuf, ")"
-    	print >> filebuf, "%s  RHS (" %(subastString),
-    	self.right.numprint()
-    	print >> filebuf, ")",
-
-	
-#For program rule at the top
-class ProgramAst(Expr):
-	def __init__(self,left):
-		self.left = left
-		print >> filebuf, "Program:"
-		print >> filebuf, "\t Procedure : main, Return Type : void"
-		self.left.numprint()
-
-
-#For Assignment statement
-class AssignAst(Expr):
-	global astString
-	def __init__(self,left,right,place,code):
-		self.left = left
-		self.right = right
-		self.place =place
-		self.code = code
-
-	def numprint(self):
-		print >> filebuf, " \n%s Asgn: \n %s LHS(" %(astString,subastString),
-		self.left.numprint()
-		print >> filebuf, ")" 
-		print >> filebuf, " %s RHS(" %(subastString), 
-		self.right.numprint()
-		print >> filebuf, ")", 
-
-
-#For UnaryOperator
-class UnaryAst(Expr):
-	global subastString
-	def __init__(self,left,code,place):
-		self.code = code
-		self.left = left
-		if self.left.type == 'int':
-			self.type = 'int'
-		elif self.left.type == 'float':
-			self.type = 'float'
-
-		self.place = place	
-
-	def numprint(self):
-		print >> filebuf, "\n %s Arith : UMINUS \n %s     LHS (" %(subastString,subastString),  #%s ) " %(left)
-		self.left.numprint()
-		print >> filebuf, ")",
-
-
-#For Relational Operator except NOT
-class RelationAst(Expr):
-	global subastString
-	global astString
-	def __init__(self,left,op,right,place,code):
-		self.left = left
-		self.op = op
-		self.right = right
-		self.place =place
-		self.code = code
-
-	def numprint(self):
-		print >> filebuf, "\n %s Condition : %s \n \t %s LHS (" %(subastString,self.op,astString),
-		self.left.numprint()
-		print >> filebuf, ")" 
-		print >> filebuf, "\t %s RHS (" %(astString),
-		self.right.numprint(),
-		print >> filebuf, ")",
-
-
-#For Procedure Definition
-class ProcedureAst(Expr):
-	def __init__(self,left,op,right,code):
-		self.left=left
-		self.op=op
-		self.right = right
-		self.code = code
-
-	def numprint(self):
-		self.right.numprint()
-
-
-#For statement list
-class SequenceAst(Expr):
-	global sequenceString
-	def __init__(self,code):
-		self.aststmt = []
-		self.code = code
-		
-	def numprint(self):
-		print >> filebuf, " \n %s Sequence Ast: \n" %(sequenceString)
-		
-		if len(self.aststmt)!=0 :
-			for ast in self.aststmt:
-				ast.numprint()		
-	
-	def pushbach(self,ast):		#For appending statements into the statement list
-		self.aststmt.append(ast)
-
-
-#For if-else statement
-class SelectionAst(Expr):
-	def __init__(self,cond,thenp,elsep,code,place):
-		self.cond = cond
-		self.thenp = thenp
-		self.elsep = elsep
-		self.code = code
-		self.place =place
-
-	def numprint(self):
-		print >> filebuf, "\n%s IF :  \n %s CONDITION (" %(astString,astString),
-		self.cond.numprint()
-		print >> filebuf, ")"
-		print >> filebuf, "%s THEN (" %(astString)
-		self.thenp.numprint()
-		print >> filebuf, ")"
-		print >> filebuf, "%s ELSE (" %(astString)
-		if self.elsep != "NULL":
-			
-			self.elsep.numprint()
-		print >> filebuf, ")"
-
-
-#For while, do-while statement
-class IterationAst(Expr):
-	global astString
-
-	def __init__(self,cond,body,doform,place,code):
-		self.cond = cond
-		self.body = body
-		self.doform = doform
-		self.place = place
-		self.code = code
-
-	def numprint(self):
-		if self.doform == "false":
-			print >> filebuf, "\n%s WHILE :  \n %s CONDITION (" %(astString,astString),
-			self.cond.numprint()
-			print >> filebuf, ")"
-			print >> filebuf, "%s BODY (" %(astString)
-			self.body.numprint()
-			print >> filebuf, ")"
-		else:
-			print >> filebuf, "\n%s DO (" %(astString)
-			self.body.numprint()
-			print >> filebuf, ")"
-			print >> filebuf, "\n%s WHILE CONDITION (" %(astString)
-			self.cond.numprint()
-			print >> filebuf, ")"	
-
-
-#For conditional expression
-class ConditionAst(Expr):
-	def __init__(self,cond,left,right):
-		self.cond = cond
-		self.left = left
-		self.right = right
-		if self.left.type == 'int':
-			self.type = 'int'
-		elif self.left.type == 'float':
-			self.type = 'float'
-
-	def numprint(self):
-		print >> filebuf, "\n%s Arith : Conditional \n %s CONDITION (" %(astString,astString),
-		self.cond.numprint()
-		print >> filebuf, ")"
-		print >> filebuf, "%s LHS (" %(astString)
-		self.left.numprint()
-		print >> filebuf, ")"
-		print >> filebuf, "%s RHS (" %(astString)
-		self.right.numprint()
-		print >> filebuf, ")"
-
-
-
-#For NOT operator
-class NotAst(Expr):
-	def __init__(self,left,place,code):
-		self.place = place
-		self.left = left
-		self.code = code
-
-	def numprint(self):
-		print >> filebuf, "\n %s Condition : NOT \n \t %s RHS (" %(subastString,astString),
-		self.left.numprint()
-		print >> filebuf, ")", 
-
-
-#For Variable names
-class NameAst(Expr):
-	global symtab
-
-	def __init__(self,left):
-		self.left = left
-		if self.left in symtab['int']:
-			self.type = "int"
-		elif self.left in symtab['float']:
-			self.type = "float"
-		self.place = self.left
-		self.code = ""
-
-	def numprint(self):
-		print >> filebuf, "Name : %s" %(self.left),
-
 
 #For numbers
-class NumberAst(Expr):
+class NumberAst():
 	def __init__(self,value,typedef):          
 		self.type = typedef
 		self.value = value
 		self.place = self.value
 		self.code = ""
+		
+class NameAst():
+    def __init__(self,left):
+        self.left=left
+        self.type="variable"
+        self.place=self.left
+        self.code=""
 
-	def numprint(self):
-		if self.type == "int":
-			print >> filebuf, "Num : %d" %(self.value),
-		else:
-			print >> filebuf, "Num : %.2f" %(self.value),
+class ArithOpAst():
+	def __init__(self,left,op,right):
+		self.left=left
+		self.op=op
+		self.right=right
+		self.place=""
+		self.code=""
+		
+class AssigOpArth():
+	def __init__(self,left,right):
+		self.left=left
+		self.right=right
+		self.place=""
+		self.code=""
