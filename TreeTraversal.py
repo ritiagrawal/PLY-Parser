b9920e886a681f774b09c1a24bc5aadf045fd9b0
ritiagrawal <ritiagrawal026@gmail.com>
2017-10-01 12:03:00 +0530

    Handling multiple pointerVar * in tree traversal

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -1,21 +1,32 @@
 class tree_traversal():
 	def __init__(self,tree):
 		#print(tree)	
+		pointers='*'
 		variable_declarations=tree.variable_declarations_obj
 	
 		variable_declaration_list=variable_declarations.variable_declaration_list_obj
 		
 		while(variable_declaration_list):
+			print("var")
 			var_list=variable_declaration_list.variable_declaration_obj.var_list_obj
 			while(var_list):
+				pointers='*'
 				try:
-					print("*",var_list.ppointer_variable_obj.pointer_variable_obj.variable_obj.name)						
+					ppointer_variable = var_list.ppointer_variable_obj
+					while(ppointer_variable):
+						try:
+							pointer_variable= ppointer_variable.pointer_variable_obj.variable_obj.name
+							print(pointers,pointer_variable)
+						except:
+							pass
+						try:
+							ppointer_variable = ppointer_variable.ppointer_variable_obj
+							pointers ='*%s'%pointers
+						except:
+							break
 				except:
 					pass
-				try:
-					print("**",var_list.ppointer_variable_obj.ppointer_variable_obj.pointer_variable_obj.variable_obj.name)						
-				except:
-					pass
+					
 				try:
 					print(var_list.variable_obj.name)						
 				except:
